6e62b7774f20c2aa4e7f226b75d6e19f10da1bca
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-04-14 15:04:46 +0200

    API fixed data and loader

--- src/components/APINavitia.js
+++ src/components/APINavitia.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import axios from 'axios'
 
-// axios.get('https://api.navitia.io/v1/coverage/sandbox/stop_areas/stop_area:RAT:SA:BASTI/lines/line:RAT:M5/departures?count=4&depth=2')
-// .then(res => console.log(res))
-
 class Navitia extends React.Component {
 
     state = {
-        transports : []
+        transports : [],
+        isloaded:false,
+        duration: 0,
+        token: 'b0b9e3a3-8f64-4941-8ba7-b62b78071d18',
+        longitude:2.3334722,
+        latitude:48.8106958,
+        longitudeArrivee:2.2922926,
+        latitudeArrivee:48.8583736
     }
 
     getTransportationTime = () => {
-        fetch('https://api.navitia.io/v1/coverage/fr-idf/journeys?from=2.3003136;48.7686144&to=2.2922926;48.8583736&key=b0b9e3a3-8f64-4941-8ba7-b62b78071d18')
+        const url = `https://api.navitia.io/v1/coverage/fr-idf/journeys?from=${this.state.longitude};${this.state.latitude}&to=${this.state.longitudeArrivee};${this.state.latitudeArrivee}&key=${this.state.token}`
+        fetch(url)
             .then (res => res.json())
-            .then (res => this.setState({transports : res}))
-            .then (res => console.log(this.state.transports.links[0].href))
+            .then (res => this.setState({transports : res, isloaded:true}))
+            .then (res => console.log(this.state.transports))
+            .then (res => console.log(this.state.transports))
+            .then (res => console.log(this.state.transports.journeys[0].sections[0].from.address.label))
+            .then (res => {
+                this.setState({duration:Math.round(this.state.transports.journeys[0].duration/60)})
+            })
     }
 
     componentDidMount(){
@@ -23,11 +33,17 @@ class Navitia extends React.Component {
     }
 
     render () {
+        
         return <div>
+            {!this.state.isloaded ? <div>Loading ...</div> : (
             <h1>
                 Test Navitia
-                <p>{this.state.transports.links.map(link => <p>link.href</p> )}</p>
-            </h1>
+                <div> Adresse de départ : {this.state.transports.journeys[0].sections[0].from.address.label}</div>
+                <div> Adresse d'arrivée : {this.state.transports.journeys[0].sections[this.state.transports.journeys[0].sections.length-1].to.address.label}</div>
+                <div> Temps de transport : {this.state.duration} minutes </div>
+                {/* <div>{this.state.transports.links.map(link => <p key={link.href}>{link.href}</p> )}</div> */}
+            </h1>)}
+            
     
         </div>
     }
