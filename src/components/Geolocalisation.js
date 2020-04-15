66d45525e287b27c085fc42add45e213de98a277
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-04-15 18:16:29 +0200

    variabilisation de la geoloc

--- src/components/Geolocalisation.js
+++ src/components/Geolocalisation.js
@@ -1,56 +1,61 @@
-import React from 'react'
-import Navitia from './APINavitia.js'
+import React from "react";
+import Navitia from "./APINavitia.js";
 
-
-
-const erreur = ( error ) => {
-    switch( error.code ) {
-        case error.PERMISSION_DENIED:
-            console.log( 'L\'utilisateur a refusé la demande' );
-            break;     
-        case error.POSITION_UNAVAILABLE:
-            console.log( 'Position indéterminée' );
-            break;
-        case error.TIMEOUT:
-            console.log( 'Réponse trop lente' );
-            break;
-    }
-    // // Function alternative
-    // alternative();
+const erreur = (error) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("L'utilisateur a refusé la demande");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("Position indéterminée");
+      break;
+    case error.TIMEOUT:
+      console.log("Réponse trop lente");
+      break;
+  }
 };
 
 class Geolocalisation extends React.Component {
-    state = {
-        longitude:2.3334722,
-        latitude:48.8106958,
-        longitudeArrivee:2.2922926,
-        latitudeArrivee:48.8583736
-    }
+  state = {
+    longitude: 2.0,
+    latitude: 48.8106958,
+    longitudeArrivee: 2.2922926,
+    latitudeArrivee: 48.8583736,
+    loaded: false,
+  };
 
-
-    getGeolocalisation = () => {
-        function callback( position ) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            console.log( lat, lng );
-            // Do stuff
-        }
-        if ( navigator.geolocation ) {
-            // On demande d'envoyer la position courante à la fonction callback
-            navigator.geolocation.getCurrentPosition( callback, erreur );
-        } 
+  getGeolocalisation = () => {
+    const callback = (position) => {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      console.log(lat, lng);
+      this.setState({ longitude: lng, latitude: lat, loaded: true });
+    };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(callback, erreur);
     }
+  };
 
-    componentDidMount(){
-        
-        this.getGeolocalisation()
-    }
+  componentDidMount() {
+    this.getGeolocalisation();
+  }
 
-    render () {
-        return (<div>
-            <Navitia longitude = {this.state.longitude} latitude = {this.state.latitude}/>
-            Geolocalisation OK</div>)
-    }
+  render() {
+    return (
+      <div>
+        {!this.state.loaded ? (
+          <div>Loading ...</div>
+        ) : (
+          <div>
+            <Navitia
+              longitude={this.state.longitude}
+              latitude={this.state.latitude}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Geolocalisation
+export default Geolocalisation;
