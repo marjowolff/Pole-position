d16bd836e14b9f68daf331f48fa9d9bfd6735beb
voinot <flovno@gmail.com>
2020-04-17 00:06:00 +0200

    add burger menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Geolocalisation from './components/Geolocalisation';
-
+import BurgerMenu from './components/BurgerMenu'
 function App() {
   return (
     <div>
       <Geolocalisation />
+      <BurgerMenu />
     </div>
   );
 }
