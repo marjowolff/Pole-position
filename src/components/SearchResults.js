eab4afa0d918e452506978a5b8e5249bef415145
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-04-20 15:19:46 +0200

    adding job list

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,23 +6,17 @@ import './SearchResults.css';
 
 
 class SearchResults extends React.Component {
-    state = {
-        title:'Développeur Web (h/f)',
-        city:'Paris 13',
-        company:'Google',
-        contractType:'CDI',
-        link:'http://www.google.fr'
-    }
+    
 
     render () {
         return (
             <div>
                 <div className="card">
-                    <h2>{this.state.title}</h2>
+                    <h2>{this.props.title}</h2>
                     <div className="cardSeparator">
-                        <p><FaBuilding />{this.state.company}</p>
-                        <p><GoLocation /> {this.state.city}</p>
-                        <p><IoMdBriefcase />{this.state.contractType}</p>
+                        <p><FaBuilding />{this.props.company}</p>
+                        <p><GoLocation /> {this.props.city}</p>
+                        <p><IoMdBriefcase />{this.props.contractType}</p>
                     </div>
                     <button className="cardButton">Voir l'offre</button>
                     
