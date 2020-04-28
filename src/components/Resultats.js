400f074e7d785f558f6a3c10d108961f447540c7
voinot <flovno@gmail.com>
2020-04-28 15:53:04 +0200

    add loader in resultats.js

--- src/components/Resultats.js
+++ src/components/Resultats.js
@@ -6,82 +6,87 @@ import BackButton from './BackButton'
 class Resultats extends React.Component {
     state = {
         jobOffers: [],
-        token : "no token",
-        jobKeyWord:"",
-        contractChoice:"",
-        natureContratChoice:""
+        token: "no token",
+        jobKeyWord: "",
+        contractChoice: "",
+        natureContratChoice: "",
+        loaded: false
     };
-    
+
     getTokenPE = () => {
-                axios({
-                  method: 'post',
-                  url: 'https://cors-anywhere.herokuapp.com/https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=%2Fpartenaire&grant_type=client_credentials&client_id=PAR_offrespoleemploi_4f4ff3756bc6ad0d21553a4cbed52fe90a09736594c0f038f134c61273aed8a1&client_secret=4c10c5635c0f9b1cce87b2687958ecca2fdd04e72bfd83262297229073f092ee&scope=application_PAR_offrespoleemploi_4f4ff3756bc6ad0d21553a4cbed52fe90a09736594c0f038f134c61273aed8a1%20api_offresdemploiv2 o2dsoffre',
-                  headers : {
-                      'Content-Type':'application/x-www-form-urlencoded',
-                         }
-                       })
-                .then(res => this.setState({token : res.data.access_token}, ()=>{this.getJobOffers()} ))
-                
-            };
-            
+        axios({
+            method: 'post',
+            url: 'https://cors-anywhere.herokuapp.com/https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=%2Fpartenaire&grant_type=client_credentials&client_id=PAR_offrespoleemploi_4f4ff3756bc6ad0d21553a4cbed52fe90a09736594c0f038f134c61273aed8a1&client_secret=4c10c5635c0f9b1cce87b2687958ecca2fdd04e72bfd83262297229073f092ee&scope=application_PAR_offrespoleemploi_4f4ff3756bc6ad0d21553a4cbed52fe90a09736594c0f038f134c61273aed8a1%20api_offresdemploiv2 o2dsoffre',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+            .then(res => this.setState({ token: res.data.access_token }, () => { this.getJobOffers() }))
+
+    };
+
     getJobOffers = () => {
-            axios({
-                method: 'get',
-                url: 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search',
-                headers: {Authorization: `Bearer ${this.state.token}`},
-                params: {
-                    motsCles : this.state.jobKeyWord,
-                    commune : 75118,
-                    typeContrat :this.state.contractChoice,
-                    natureContrat : this.state.natureContratChoice
-                     }
-                   })
-                .then(res => this.setState({jobOffers: res.data.resultats}))
-           }
-            
-    handleKeyWords=()=>{
-        if (this.props.location.data.userKeyWord !== this.state.jobKeyWord){
-            this.setState({jobKeyWord:this.props.location.data.userKeyWord})
+        axios({
+            method: 'get',
+            url: 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search',
+            headers: { Authorization: `Bearer ${this.state.token}` },
+            params: {
+                motsCles: this.state.jobKeyWord,
+                commune: 75118,
+                typeContrat: this.state.contractChoice,
+                natureContrat: this.state.natureContratChoice
+            }
+        })
+            .then(res => this.setState({ jobOffers: res.data.resultats, loaded: true }))
+    }
+
+    handleKeyWords = () => {
+        if (this.props.location.data.userKeyWord !== this.state.jobKeyWord) {
+            this.setState({ jobKeyWord: this.props.location.data.userKeyWord })
         }
     }
-    handleContractChoice = () =>{
-        let contractTab=[]
-        if (this.props.location.data.selectCDI === true){
+    handleContractChoice = () => {
+        let contractTab = []
+        if (this.props.location.data.selectCDI === true) {
             contractTab.push("CDI")
-        } 
-        if (this.props.location.data.selectCDD === true){
+        }
+        if (this.props.location.data.selectCDD === true) {
             contractTab.push("CDD")
         }
-        if (this.props.location.data.selectINTERIM === true){
+        if (this.props.location.data.selectINTERIM === true) {
             contractTab.push("MIS")
         }
-        let contractList=contractTab.join(",")
-        this.setState({contractChoice : contractList})
-        }
-    handleNatureContrat =() =>{
-        if (this.props.location.data.selectApprenti === true){
-            this.setState({natureContratChoice : "E2"})
-        } 
+        let contractList = contractTab.join(",")
+        this.setState({ contractChoice: contractList })
     }
-           
+    handleNatureContrat = () => {
+        if (this.props.location.data.selectApprenti === true) {
+            this.setState({ natureContratChoice: "E2" })
+        }
+    }
+
 
     componentDidMount() {
         this.handleKeyWords()
         this.handleContractChoice()
-        this.handleNatureContrat() 
-        this.getTokenPE()  
-      }
+        this.handleNatureContrat()
+        this.getTokenPE()
+    }
 
-    render(){
-        return(
-        <div>
-            <BackButton />
-            {this.state.jobOffers.map(offer => (
-                <div key={offer.id}>
-                    <SearchResults title={offer.intitule} city={offer.lieuTravail.libelle} company={offer.entreprise !== undefined && offer.entreprise.nom} contractType={offer.typeContrat}/>
+    render() {
+        return (
+            <div>
+                <BackButton />
+                <div>
+                    {!this.state.loaded ? (<div>Loading ...</div>) : (
+                        <div>{this.state.jobOffers.map(offer => (
+                            <div key={offer.id}>
+                                <SearchResults title={offer.intitule} city={offer.lieuTravail.libelle} company={offer.entreprise !== undefined && offer.entreprise.nom} contractType={offer.typeContrat} />
+                            </div>
+                        ))}</div>
+                    )}
                 </div>
-            ))}
-        </div>
+            </div>
         );
     }
 }
