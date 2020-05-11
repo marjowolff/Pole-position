64c5a86cb46bbd7cbac3eac9794e16e13c7c2b1b
Céline Cottier <cottier.celine@gmail.com>
2020-05-11 15:41:40 +0200

    added color code for travel times

--- src/components/ShowResults.js
+++ src/components/ShowResults.js
@@ -6,29 +6,22 @@ import { IoMdBriefcase } from 'react-icons/io'
 import './SearchResults.css';
 
 
-
-
 class ShowResults extends React.Component {
-    
-
     render () {
-        //console.log(this.props.lienOffrePE)
+        const {title, company, city, contractNature, contractType, tempsTrajet, lienOffrePE} = this.props
         return (
             <div>
                 <div className="card">
-                    <h2>{this.props.title}</h2>
+                    <h2>{title}</h2>
                     <div className="cardSeparator">
-                        {this.props.company ? 
-                        <p><FaBuilding />{this.props.company}</p> : <p><FaBuilding />Entreprise non renseignée</p>}
-                        <p><GoLocation /> {this.props.city}</p>
+                        {company ? 
+                        <p><FaBuilding />{company}</p> : <p><FaBuilding />Entreprise non renseignée</p>}
+                        <p><GoLocation /> {city}</p>
                         {this.props.contractNature === "Contrat apprentissage" ?
-                        <p><IoMdBriefcase />{this.props.contractNature}</p> : <p><IoMdBriefcase />{this.props.contractType}</p>}
-                        <p id="FaBus"><FaBus /> {this.props.tempsTrajet} min </p>
+                        <p><IoMdBriefcase />{contractNature}</p> : <p><IoMdBriefcase />{contractType}</p>}
+                        <p id="FaBus" className={tempsTrajet < 20 ? "inf20" : (tempsTrajet < 30 ? "inf30" : (tempsTrajet < 45 ? "inf45": (tempsTrajet < 60 ? "inf60": (tempsTrajet <75 ? "inf75" : "sup75")))) }><FaBus /> {tempsTrajet} min </p>
                     </div>
-                    {/* <p id="FaBus"><FaBus /> 20 min</p> */}
-                    { /*  <NavitiaTime longitudeDepart={this.props.longitudeDepart} latitudeDepart={this.props.latitudeDepart} longitudeArrivee={this.props.longitudeArrivee} latitudeArrivee={this.props.latitudeArrivee} liftDuration={this.props.liftDuration}/>*/}
-                    <a href={this.props.lienOffrePE} target="blanck" ><button className="cardButton">Voir l'offre</button></a>
-                    
+                    <a href={lienOffrePE} target="blanck" ><button className="cardButton">Voir l'offre</button></a>  
                 </div>
             </div>
         )
