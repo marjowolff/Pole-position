c959ce29491d76bb804c57f43e8145d6252619fa
Céline Cottier <cottier.celine@gmail.com>
2020-05-12 18:31:03 +0200

    Token stored on session Storage, get a new token if API PE call is not possible

--- src/components/OffresPE.js
+++ src/components/OffresPE.js
@@ -9,19 +9,25 @@ import Loader from "./Loader"
 class OffresPE extends React.Component {
     state = {
         jobOffers: [],
-        token : "no token",
         jobKeyWord:"",
         contractChoice:"",
         natureContratChoice:"",
         loadedUserChoices : false,
         tokenLoaded : false,
-        loaded: false,
+        offersloaded: false,
     };
 
     getTokenPE = () => {
         let tokenFromStor = sessionStorage.getItem('tokenStor')
 
-        if( this.state.token === "no token"){
+        if( !tokenFromStor ){
+            this.getTokenPEAxios()
+        } else {
+            this.setState({ tokenLoaded : true })
+        }
+    };
+
+    getTokenPEAxios = () => {
         axios({
             method: 'post',
             url: 'https://cors-anywhere.herokuapp.com/https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=%2Fpartenaire&grant_type=client_credentials&client_id=PAR_offrespoleemploi_4f4ff3756bc6ad0d21553a4cbed52fe90a09736594c0f038f134c61273aed8a1&client_secret=4c10c5635c0f9b1cce87b2687958ecca2fdd04e72bfd83262297229073f092ee&scope=application_PAR_offrespoleemploi_4f4ff3756bc6ad0d21553a4cbed52fe90a09736594c0f038f134c61273aed8a1%20api_offresdemploiv2 o2dsoffre',
@@ -33,15 +39,12 @@ class OffresPE extends React.Component {
                 sessionStorage.setItem('tokenStor', res.data.access_token)
                 this.setState({ tokenLoaded : true })
             })
-            //.then(res => this.setState({ token: res.data.access_token } /*, () => { this.getJobOffers() } */))
-        } 
-    };
-
-    //sessionStorage.setItem('tokenStor', 'res.data.resultats'
+    }
 
     // https://cors-anywhere.herokuapp.com/
 
-    getJobOffers = () => {
+  
+    getJobOffersAxios = () => {
         let tokenFromStor = sessionStorage.getItem('tokenStor')
         axios({
             method: 'get',
@@ -58,8 +61,17 @@ class OffresPE extends React.Component {
                 //range: "0-10"
             }
         })
-            .then(res => this.setState({ jobOffers: res.data.resultats, loaded: true }))
+            .then(res => this.setState({ jobOffers: res.data.resultats, offersloaded: true }) , () => this.errorJobOffersAxios())
     }
+
+    errorJobOffersAxios = (error) => {
+        console.log("pas de token !!!!!")
+        this.getTokenPEAxios()
+    }
+
+
+
+
 
     handleKeyWords = () => {
         if (this.props.location.data.userKeyWord !== this.state.jobKeyWord) {
@@ -90,46 +102,34 @@ class OffresPE extends React.Component {
         this.handleKeyWords()
         this.handleContractChoice()
         this.handleNatureContrat()
-        this.setState({ loadedUserChoices: true } /* ,() => { this.getTokenPE() }*/)
+        this.setState({ loadedUserChoices: true })
     }
 
-    tokenStorage = () => {
-        sessionStorage.setItem('tokenStor', 'rien à stocker')
-    }
-    getTokenStorage = () => {
-        let tokenFromStor = sessionStorage.getItem('tokenStor')
-        console.log(`Le token du cache : ${tokenFromStor}`)
-    }
-     
+
+
     componentDidMount() {
         this.handleUserChoices()
         this.getTokenPE()
-        //this.tokenStorage()
-        //this.getTokenPE()  
-        //this.getJobOffers()
       }
     
     componentDidUpdate() {
-        if(this.state.tokenLoaded){
-        this.getTokenStorage()
-        }
         if (this.state.loadedUserChoices && this.state.tokenLoaded){
-            this.getJobOffers()
-            this.setState({loadedUserChoices : false }) 
+            this.getJobOffersAxios()
+            this.setState({tokenLoaded : false }) 
         }
     }
+
 
     render() {
           //console.log(`props temps trajet max :${this.props.location.data.tempsTrajetMax}`)
           //console.log(this.state.jobOffers)
-          console.log(this.state.token)
+          console.log(`le token du cache ${sessionStorage.getItem('tokenStor')}`)
           
         return (
             <div>
                 <BackButton />
-                {/* <NavitiaTime longitudeDepart={this.state.longitudeDepart} latitudeDepart={this.state.latitudeDepart} longitudeArrivee={this.state.longitudeArrivee} latitudeArrivee={this.state.latitudeArrivee} /> */}
                 <div>
-                    {!this.state.loaded ? (<Loader />) : (
+                    {!this.state.offersloaded ? (<Loader />) : (
                        
                        this.state.jobOffers.length > 0 ?  (
                             <TempsTrajetNavitia jobOffers={this.state.jobOffers} longitudeDepart={this.props.location.data.longitudeDepart} latitudeDepart={this.props.location.data.latitudeDepart} tempsTrajetMax={this.props.location.data.tempsTrajetMax}/>
