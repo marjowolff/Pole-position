5762aa069681d7567b9b0529296b0abebf6cc4b1
voinot <flovno@gmail.com>
2020-05-15 08:58:02 +0200

    add image for faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,13 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Faq.css'
-
+import faqImg from './Images/faqImg'
 
 class Faq extends Component {
-    render () {
-        return <div className=''>
-          <h1 className="FaqTitle">F.A.Q</h1>
-        </div>
-      }
+  render() {
+    return <div className='ImgQuest'>
+      <header>
+        <h1 className="FaqTitle">F.A.Q</h1>
+      </header>
+<div> 
+  <img src={faqImg} atl="Logo faq" className="FaqImg"/>
+</div>
+
+    </div>
+  }
 }
 
 
--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -1,21 +1,19 @@
 import React from 'react'
-
 import RechercheMotsCles from './RechercheMotsCles'
 import VotreAdresse from './VotreAdresse'
 import VotreTempsTrajet from './VotreTempsTrajet'
 import BoutonValider from './BoutonValider'
 import ChoixContrat from './ChoixContrat'
-import footer from './Images/footer.png'
 
 
 
 class Recherche extends React.Component {
   state = {
     userKeyWord: "",
-    selectCDI:false,
-    selectCDD:false,
-    selectINTERIM:false,
-    selectApprenti:false,
+    selectCDI: false,
+    selectCDD: false,
+    selectINTERIM: false,
+    selectApprenti: false,
     tempsTrajetMax: "10000000000000",
     longitudeDepart: 2.0,
     latitudeDepart: 48.0
@@ -25,54 +23,52 @@ class Recherche extends React.Component {
     this.setState({ userKeyWord: userInput })
   }
   handleCDI = () => {
-    this.setState({selectCDI : !this.state.selectCDI})
+    this.setState({ selectCDI: !this.state.selectCDI })
   }
   handleCDD = () => {
-    this.setState({selectCDD : !this.state.selectCDD}) 
+    this.setState({ selectCDD: !this.state.selectCDD })
   }
   handleINTERIM = () => {
-    this.setState({selectINTERIM: !this.state.selectINTERIM}) 
+    this.setState({ selectINTERIM: !this.state.selectINTERIM })
   }
   handleApprenti = () => {
-    this.setState({selectApprenti: !this.state.selectApprenti}) 
+    this.setState({ selectApprenti: !this.state.selectApprenti })
   }
-  handleTempsTrajetMax =(event) =>{
+  handleTempsTrajetMax = (event) => {
     const userSelect = event.target.value;
-    this.setState({tempsTrajetMax: userSelect})
+    this.setState({ tempsTrajetMax: userSelect })
 
   }
-  
-  handleLiftCoordDepart = (lng,lat) => {
-    console.log('Dans le Lift State lng est ',lng)
-    console.log('Dans le Lift State state lng est ',this.state.longitudeDepart)
-    this.setState({longitudeDepart: lng,latitudeDepart:lat}) 
-  }
- 
 
-  render (){
+  handleLiftCoordDepart = (lng, lat) => {
+    console.log('Dans le Lift State lng est ', lng)
+    console.log('Dans le Lift State state lng est ', this.state.longitudeDepart)
+    this.setState({ longitudeDepart: lng, latitudeDepart: lat })
+  }
+
+
+  render() {
     //console.log(`State de tempsTrajetMax : ${this.state.tempsTrajetMax}`)
     return (
-      
+
       <div className="divPageRecherche">
 
         <h1 className="Title">Bienvenue !</h1>
-        <RechercheMotsCles userKeyWord={this.state.userKeyWord} handleChangeJobReve={this.handleChangeJobReve}/> 
+        <RechercheMotsCles userKeyWord={this.state.userKeyWord} handleChangeJobReve={this.handleChangeJobReve} />
         {/* <div>Votre adresse dans le composant Parent {this.state.longitudeDepart} et {this.state.latitudeDepart}</div> */}
-        <VotreAdresse long={this.state.longitudeDepart} lat={this.state.latitudeDepart} handleLiftCoordDepart={this.handleLiftCoordDepart}/>
-        <VotreTempsTrajet handleTempsTrajetMax={this.handleTempsTrajetMax}/>
-        <ChoixContrat selectCDI={this.state.selectCDI} handleCDI={this.handleCDI} selectCDD={this.state.selectCDD} handleCDD={this.handleCDD} selectINTERIM={this.state.selectINTERIM} handleINTERIM={this.handleINTERIM} selectApprenti={this.state.selectApprenti} handleApprenti={this.handleApprenti}/>
-        
-<footer>
-<div className="Footer">
-  <BoutonValider userKeyWord={this.state.userKeyWord} tempsTrajetMax={this.state.tempsTrajetMax} selectCDI={this.state.selectCDI} selectCDD={this.state.selectCDD} selectINTERIM={this.state.selectINTERIM} selectApprenti={this.state.selectApprenti} longitudeDepart={this.state.longitudeDepart} latitudeDepart={this.state.latitudeDepart}/>  
-  
-            
-        </div>
-      </footer>
+        <VotreAdresse long={this.state.longitudeDepart} lat={this.state.latitudeDepart} handleLiftCoordDepart={this.handleLiftCoordDepart} />
+        <VotreTempsTrajet handleTempsTrajetMax={this.handleTempsTrajetMax} />
+        <ChoixContrat selectCDI={this.state.selectCDI} handleCDI={this.handleCDI} selectCDD={this.state.selectCDD} handleCDD={this.handleCDD} selectINTERIM={this.state.selectINTERIM} handleINTERIM={this.handleINTERIM} selectApprenti={this.state.selectApprenti} handleApprenti={this.handleApprenti} />
+
+        <footer>
+          <div className="Footer">
+            <BoutonValider userKeyWord={this.state.userKeyWord} tempsTrajetMax={this.state.tempsTrajetMax} selectCDI={this.state.selectCDI} selectCDD={this.state.selectCDD} selectINTERIM={this.state.selectINTERIM} selectApprenti={this.state.selectApprenti} longitudeDepart={this.state.longitudeDepart} latitudeDepart={this.state.latitudeDepart} />
+          </div>
+        </footer>
       </div>
-      
+
     );
-  
+
+  }
 }
- } 
-  export default Recherche;
+export default Recherche;
